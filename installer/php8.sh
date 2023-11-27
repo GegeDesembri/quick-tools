efe0941c927b880048bf1cb468dbd486b090733c
Gege Desembri <62755680+GegeDesembri@users.noreply.github.com>
2023-11-27 14:03:09 +0700

    Update php8.sh

--- installer/php8.sh
+++ installer/php8.sh
@@ -8,4 +8,4 @@ wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add -
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
 apt-get update -y
 apt-get install -y php${php_version}
-apt-get install -y php${php_version}-xml php${php_version}-cli php${php_version}-curl
+apt-get install -y php${php_version}-xml php${php_version}-cli php${php_version}-curl php${php_version}-mbstring
